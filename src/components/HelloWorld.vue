fd722a418f7f83899bc5209047aed68bb8ef6410
Serdar Mamadzhanov <sitior@me.com>
2024-03-19 19:37:36 -0400

    Add Greeting to HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <h1>THIS IS MY NEW PERSONAL SITE</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
